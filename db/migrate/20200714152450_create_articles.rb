f7445cfaddee15d4f039ec9be624dc05341f86a2
souken-b <yuto430scuderia@gmail.com>
2020-07-30 15:04:51 +0900

    articleテーブルのurlカラムをnull:falseに変更

--- db/migrate/20200714152450_create_articles.rb
+++ db/migrate/20200714152450_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :url
+      t.string :url, null: false
       t.integer :check
       t.string :comment
 
