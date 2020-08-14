84c5f5bf9738e285240ceacaf1c31985b5171cbb
souken-b <yuto430scuderia@gmail.com>
2020-08-14 14:03:16 +0900

    client取得のmoduleを作成

---
+++ app/services/line_bot/messages/fetch_line_client.rb
@@ -0,0 +1,12 @@
+module LineBot
+  module Messages
+    class FetchLineClient
+      def client
+        client = Line::Bot::Client.new do |config|
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+          config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+        end
+      end
+    end
+  end
+end
